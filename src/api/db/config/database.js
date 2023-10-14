b9d9f29043dfcc1d13f924b14b4396854c04f1e1
ronaldidro <rjdiazi10@gmail.com>
2023-10-14 00:29:17 -0500

    removed seeds storage config

--- src/api/db/config/database.js
+++ src/api/db/config/database.js
@@ -6,22 +6,16 @@ export default {
   development: {
     use_env_variable: "DEV_DATABASE_URL",
     dialect: "postgres",
-    seederStorage: "sequelize",
-    seederStorageTableName: "seeders",
     migrationStorageTableName: "migrations",
   },
   test: {
     use_env_variable: "TEST_DATABASE_URL",
     dialect: "postgres",
-    seederStorage: "sequelize",
-    seederStorageTableName: "seeders",
     migrationStorageTableName: "migrations",
   },
   production: {
     use_env_variable: "DATABASE_URL",
     dialect: "postgres",
-    seederStorage: "sequelize",
-    seederStorageTableName: "seeders",
     migrationStorageTableName: "migrations",
   },
 };
--- src/api/db/index.js
+++ src/api/db/index.js
@@ -34,23 +34,6 @@ const migrationConf = {
   logger: console,
 };
 
-const seedConf = {
-  migrations: {
-    glob: "src/api/db/seeders/*.cjs",
-    resolve: ({ name, path, context }) => {
-      const seed = import(path);
-      return {
-        name,
-        up: async () => (await seed).up(context),
-        down: async () => (await seed).down(context),
-      };
-    },
-  },
-  storage: new SequelizeStorage({ sequelize, modelName: "seeders" }),
-  context: sequelize.getQueryInterface(),
-  logger: console,
-};
-
 const runMigrations = async () => {
   const migrator = new Umzug(migrationConf);
   const migrations = await migrator.up();
@@ -59,19 +42,10 @@ const runMigrations = async () => {
   });
 };
 
-const runSeeds = async () => {
-  const seeder = new Umzug(seedConf);
-  const seeds = await seeder.up();
-  info("Seeds up to date", {
-    files: seeds.map((seed) => seed.name),
-  });
-};
-
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
     await runMigrations();
-    await runSeeds();
     info("Connected to the database");
   } catch (err) {
     error(`Failed to connect to the database: ${err}`);
