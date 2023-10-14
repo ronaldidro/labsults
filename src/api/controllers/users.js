d4444f82484ad6b4c7614bf4dd4dfdb2e0557aa8
ronaldidro <rjdiazi10@gmail.com>
2023-10-14 00:43:32 -0500

    updated user finder middleware

--- src/api/controllers/users.js
+++ src/api/controllers/users.js
@@ -11,6 +11,8 @@ const userFinder = async (req, res, next) => {
     attributes: { exclude: ["password", "createdAt", "updatedAt"] },
   });
 
+  if (!req.user) return res.status(404).end();
+
   next();
 };
 
@@ -22,38 +24,28 @@ UsersRouter.get("/", async (req, res) => {
   res.json(users);
 });
 
-UsersRouter.get("/:id", userFinder, async (req, res) => {
-  if (req.user) {
-    res.json(req.user);
-  } else {
-    res.status(404).end();
-  }
-});
+UsersRouter.get("/:id", userFinder, async (req, res) => res.json(req.user));
 
 UsersRouter.post("/", async (req, res) => {
   const passwordHash = await getPasswordHash(req.body.password);
   const user = await User.create({ ...req.body, password: passwordHash });
+
   res.json(user);
 });
 
 UsersRouter.patch("/:id", userFinder, async (req, res) => {
-  if (req.user) {
-    let passwordHash = "";
+  let passwordHash = "";
 
-    if (req.body.password)
-      passwordHash = await getPasswordHash(req.body.password);
+  if (req.body.password)
+    passwordHash = await getPasswordHash(req.body.password);
 
-    const password = req.body.password ? passwordHash : req.user.password;
+  const password = req.body.password ? passwordHash : req.user.password;
 
-    await req.user.update({ ...req.body, password });
-    res.json(req.user);
-  } else {
-    res.status(404).end();
-  }
+  await req.user.update({ ...req.body, password });
+  res.json(req.user);
 });
 
 UsersRouter.delete("/:id", userFinder, async (req, res) => {
-  if (req.user) await req.user.destroy();
-
+  await req.user.destroy();
   res.status(204).end();
 });
