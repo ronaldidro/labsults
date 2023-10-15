7782de44bced1fb2f2fcb4ca24bb3ccfce3f8960
ronaldidro <rdiaz@digital.gob.pe>
2023-10-15 14:03:17 -0500

    added user roles to token

--- src/api/controllers/authentication.js
+++ src/api/controllers/authentication.js
@@ -1,26 +1,27 @@
 import bcrypt from 'bcrypt'
 import { Router } from 'express'
 import jwt from 'jsonwebtoken'
-import User from '../models/user.js'
+import { Role, User } from '../models/index.js'
 import { SECRET } from '../utils/config.js'
 
 export const AuthenticationRouter = Router()
 
 AuthenticationRouter.post('/', async (req, res) => {
   const { username, password } = req.body
-  const user = await User.findOne({ where: { username } })
+
+  const user = await User.findOne({
+    where: { username },
+    attributes: ['id', 'document_number', 'last_names', 'first_names', 'password'],
+    include: { model: Role, attributes: ['id', 'name'], through: { attributes: [] } }
+  })
+
   const passwordCorrect = user ? await bcrypt.compare(password, user.password) : false
 
   if (!(user && passwordCorrect)) return res.status(401).json({ error: 'invalid username or password' })
 
-  const userForToken = {
-    id: user.id,
-    document_number: user.document_number,
-    first_names: user.first_names,
-    last_names: user.last_names
-  }
+  delete user.dataValues.password
 
-  const token = jwt.sign(userForToken, SECRET, { expiresIn: '1d' })
+  const token = jwt.sign(user.dataValues, SECRET, { expiresIn: '1d' })
 
   res.status(200).send({ token })
 })
